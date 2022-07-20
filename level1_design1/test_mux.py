bf2843f2b7724c69ef6549720d4509dd2f160694
Ashwanth1399 <ashvanth1399@gmail.com>
2022-07-20 14:42:52 +0000

    updated test file

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,5 +6,7 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
-inp0=4;
+inp0=random.randint(0,3)
+inp1=random.randint(0,3)
+inp2=random.randint(0,3)
     cocotb.log.info('##### CTB: Develop your test here ########')
