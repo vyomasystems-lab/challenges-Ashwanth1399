19cc2175f6c6312d3869f7fc6ad964b5d9c96037
Ashwanth1399 <ashvanth1399@gmail.com>
2022-07-23 15:50:36 +0000

    updated

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,7 +6,38 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
-inp0=random.randint(0,3)
-inp1=random.randint(0,3)
-inp2=random.randint(0,3)
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    dut.inp0.value=1
+    dut.inp1.value=2
+    dut.inp2.value=3
+    dut.inp3.value=1
+    dut.inp4.value=2
+    dut.inp5.value=3
+    dut.inp6.value=1
+    dut.inp7.value=2
+    dut.inp8.value=3
+    dut.inp9.value=1
+    dut.inp10.value=2
+    dut.inp11.value=3
+    dut.inp12.value=1
+    dut.inp13.value=2
+    dut.inp14.value=3
+    dut.inp15.value=1
+    dut.inp16.value=2
+    dut.inp17.value=3
+    dut.inp18.value=1
+    dut.inp19.value=2
+    dut.inp20.value=3
+    dut.inp21.value=1
+    dut.inp22.value=2
+    dut.inp23.value=3
+    dut.inp24.value=1
+    dut.inp25.value=2
+    dut.inp26.value=3
+    dut.inp27.value=1
+    dut.inp28.value=2
+    dut.inp29.value=3
+    dut.inp30.value=1
+    for i in range (31):
+        dut.sel.value=i
+        await Timer(2, units='ns')
+        dut.log.info(f' DUT={int(dut.out.value):31}')
