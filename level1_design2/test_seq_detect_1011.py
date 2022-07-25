fc81288ea400f5f8dfe3758b11d10829abca50c8
Ashwanth1399 <ashvanth1399@gmail.com>
2022-07-25 14:40:17 +0000

    final tested

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -22,8 +22,28 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)  
     dut.reset.value = 0
     await FallingEdge(dut.clk)
-    dut.inp_bit.value=inp_bit[0]
+    
     for i in range(8):
+        dut.inp_bit.value=inp_bit[i]
         await RisingEdge(dut.clk)
-        dut.inp_bit.value=inp_bit[i+1]
         cocotb.log.info(f'input={dut.inp_bit.value} Epected_out={exp_out[i]:2}  DUT={int(dut.seq_seen.value):2}')
+        assert dut.seq_seen.value == exp_out[i], "Overlap sequence not detected"
+
+@cocotb.test()
+async def test_seq_bug2(dut):
+    """Test for seq detection """
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+    inp_bit=[1,0,1,1,1,0,1,1,1]
+    exp_out=[0,0,0,0,1,0,0,0,1]
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)  
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+    
+    for i in range(9):
+        dut.inp_bit.value=inp_bit[i]
+        await RisingEdge(dut.clk)
+        cocotb.log.info(f'input={dut.inp_bit.value} Epected_out={exp_out[i]:2}  DUT={int(dut.seq_seen.value):2}')
+        assert dut.seq_seen.value == exp_out[i], "sequence not detected"
